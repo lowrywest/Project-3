367e1b797ad497aff8daf6bc1db68b1078328dfc
Lowry West <4lowrywest@gmail.com>
2019-03-27 13:37:37 -0500

    MesoEqual Constructor

--- MesoEqual.java
+++ MesoEqual.java
@@ -1,10 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MesoEqual
 {
+	/*
+	 * Global String variable to hold the STID value from the constructor
+	 */
 	private String StiD;
+	private Map<String, Integer> equalAsciiVals;
 	
 	public MesoEqual(String Stid)
 	{
 		StiD=Stid;
+		equalAsciiVals= new HashMap<String, Integer>();
 	}
+
+	public HashMap<String, Integer> calAsciiEqual() 
+	{
+		
+		
+		return null;
+	}
+	
 }
