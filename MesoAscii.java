4232f0f4de1bf8618caf4517ca241b018c61e5d7
Lowry West <4lowrywest@gmail.com>
2019-03-27 13:17:38 -0500

    fixing CalAverage

--- MesoAscii.java
+++ MesoAscii.java
@@ -13,13 +13,30 @@ public class MesoAscii extends MesoAsciiAbstract
 	{
 		vals=Stid.getStID().toCharArray();
 	}
-	
+	/*
+	 * Defining the abstract method from the abstract class and seeing wether to return the floor or ceil value.
+	 */
 	@Override
 	public int calAverage()
 	{
-		double sum= vals[0]+vals[1]+vals[2]+vals[3];
-		
-		return (int) (sum/4);
+		double avg= vals[0]+vals[1]+vals[2]+vals[3]/4.0;
+		/*
+		 * comparing the decimal point value of the average to see whether to return the ceil or floor value
+		 */
+		if(avg-Math.floor(avg)< .5)
+			{
+			/*
+			 * Setting the third array value to the rounded down average if the decimal value of the average is less than 0.5
+			 */
+			return (int)Math.floor(avg);
+			}
+		else
+		{
+			/*
+			 * Setting the third array value to the rounded up average if the decimal value of the average is greater than or equal to 0.5
+			 */
+			return (int)Math.ceil(avg);
+		}
 	}
 	
 }
