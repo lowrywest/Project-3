9f9c247ff05d1f66f338821313e075835e61da70
Lowry West <4lowrywest@gmail.com>
2019-03-27 13:10:51 -0500

    Inital commits

---
+++ MesoAscii.java
@@ -0,0 +1,24 @@
+
+public class MesoAscii extends MesoAsciiAbstract
+{
+	
+	/*
+	 * Creating a global array to hold all the char values in the Stid String 
+	 */
+	private char[] vals;
+	
+	/*
+	 * Creating the 
+	 */
+	public MesoAscii(String Stid)
+	{
+		vals=Stid.toCharArray();
+	}
+	
+	@Override
+	int calAverage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
