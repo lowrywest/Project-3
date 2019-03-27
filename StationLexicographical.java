fd2dfc82e642a54ddf81cc484f6310caca5d2d05
Lowry West <4lowrywest@gmail.com>
2019-03-27 14:46:00 -0500

    Station Lexicographical class

--- StationLexicographical.java
+++ StationLexicographical.java
@@ -5,6 +5,11 @@ import java.util.TreeMap;
 public class StationLexicographical extends MesoSortedAbstract
 {
 	private TreeMap<String, Integer> sorted= new TreeMap<String, Integer>();
+	
+	public StationLexicographical(HashMap<String, Integer> AsciiVals)
+	{
+		
+	}
 	@Override
 	Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) 
 	{
