7c3de24698af0e37403f817303bcd1eb52fc25dd
Lowry West <4lowrywest@gmail.com>
2019-03-27 13:12:14 -0500

    MesoAscii constructor

--- MesoAscii.java
+++ MesoAscii.java
@@ -6,7 +6,19 @@ public class MesoAscii extends MesoAsciiAbstract
 	 * Creating a global array to hold all the char values in the Stid String 
 	 */
 	private char[] vals;
+	/*
+	 * Creating the 
+	 */
+	public MesoAscii(String Stid)
+	{
+		vals=Stid.toCharArray();
+	}
 	
+	@Override
+	int calAverage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	
 }
