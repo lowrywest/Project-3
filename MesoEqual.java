73ac222eee7b603455480bb064ca329d941bd233
Lowry West <4lowrywest@gmail.com>
2019-03-27 13:46:05 -0500

    Reading in all STID values from the mesonet file

--- MesoEqual.java
+++ MesoEqual.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,15 +12,71 @@ public class MesoEqual
 	 */
 	private String StiD;
 	private Map<String, Integer> equalAsciiVals;
+	private ArrayList<String> keyValues= new ArrayList<String>();
+	
+	public void readInfo() throws IOException
+	{
+			/*
+			 * holds the number of the STID value being read in 
+			 */
+			int loc=0;
+			/*
+			 * holds the current STID value
+			 */
+			String temp="";
+			/*
+			 * Constructing the Buffered reader
+			 */
+			BufferedReader STIDVals= new BufferedReader(new FileReader("Mesonet.txt"));
+			/*
+			 * Priming reads to skip the first 3 lines of Mesonet.txt
+			 */
+			STIDVals.readLine();
+			STIDVals.readLine();
+			STIDVals.readLine();
+			
+			/*
+			 * loop through lines of the Mesonet.txt file
+			 */
+			while(loc<120)
+			{
+				/*
+				 * setting temp to the entire line at that location
+				 */
+				temp=STIDVals.readLine();
+				/*
+				 * changing the value from the entire line to just the STID value
+				 */
+				temp=temp.substring(1, 5);
+				/*
+				 * adding the STID value to the ArrayList
+				 */
+				keyValues.add(temp);
+				/*
+				 * Incrementing the location being read in
+				 */
+				loc++;
+			}
+		/*
+		 * Closing the BufferReader 
+		 */
+		STIDVals.close();
+	}
 	
 	public MesoEqual(String Stid)
 	{
 		StiD=Stid;
 		equalAsciiVals= new HashMap<String, Integer>();
 	}
+	
+	
 
 	public HashMap<String, Integer> calAsciiEqual() 
 	{
+		for(String StiD: AsciiVal.keySet())
+		{
+			
+		}
 		
 		
 		return null;
